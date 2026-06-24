a8c6b79fcc3e49407a30df4b314eb79aa0308310
Samir Latifi <samirka@outlook.com>
2026-06-23 18:23:59 -0700

    Decouple app versions from package releases

--- studio/src/version.ts
+++ studio/src/version.ts
@@ -1,4 +1,4 @@
-export const STUDIO_VERSION = '0.24.7'
+export const STUDIO_VERSION = '0.9.0'
 export const STUDIO_PROTOCOL_VERSION = 'anip/0.24'
 
 export const STUDIO_VERSION_LABEL = `Studio v${STUDIO_VERSION}`
